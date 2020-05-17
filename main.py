f975f4d333d60e6958cff7c95a6f67a44f62255e
cloudlakecho <cloudlakecho@users.noreply.github.com>
2020-05-16 20:27:12 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+# COVID-19 Open Research Dataset Challenge (CORD-19)
+# What has been published about information sharing and inter-sectoral collaboration?
+
+# Cloud Cho in May 5, 2020
+
+# Reference:
+#   Task: https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge/tasks?taskId=583
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+
+# (1) Input
+
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        # print(os.path.join(dirname, filename))
+
+
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
