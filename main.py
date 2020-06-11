e6ec8f8be80b852b13617feccbb8e9b468840acd
cloudlakecho <cloudlakecho@users.noreply.github.com>
2020-06-10 23:42:39 -0700

    paper analysis

--- main.py
+++ main.py
@@ -11,21 +11,48 @@
 # Reference:
 #   Task: https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge/tasks?taskId=583
 
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
 
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 
-# (1) Input
+# (1) Start chatbot
+
+
+# (2) Input of each paper
 
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         # print(os.path.join(dirname, filename))
 
 
+# (3) Make summary of paper
+# paper title | total page | summary
+#
+# Comparing papers
+# Latent Semantic Indexing Model using Truncated SVD -> Longest common
+#   subsequence problem or Longest common substring problem
 
-# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+# (4) Receive question
+# Make list of synomym of key word
+#   How chatbot understand question?
+
+
+# (5) Find document using summary
+# Check paper title
+# Check summary
+#   Yes: read document
+#   No:
+
+
+# (6) Read document to answer the question
+#   How chatbot answer?
---
+++ topic.py
@@ -0,0 +1,195 @@
+
+# topic.py - Latent Semantic Indexing Model using Truncated SVD
+#
+
+# Reference:
+#   https://www.kaggle.com/thebrownviking20/topic-modelling-with-spacy-and-scikit-learn
+
+# Usual imports
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import string
+import matplotlib.pyplot as plt
+from sklearn.decomposition import NMF, LatentDirichletAllocation, TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.manifold import TSNE
+import concurrent.futures
+import time
+import pyLDAvis.sklearn
+from pylab import bone, pcolor, colorbar, plot, show, rcParams, savefig
+import warnings
+warnings.filterwarnings('ignore')
+
+
+import os
+print(os.listdir("../input"))
+
+# Plotly based imports for visualization
+from plotly import tools
+import plotly.plotly as py
+from plotly.offline import init_notebook_mode, iplot
+init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+
+# spaCy based imports
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from spacy.lang.en import English
+
+# Loading data
+wines = pd.read_csv('metadata.csv')
+wines.head()
+
+# Creating a spaCy object
+nlp = spacy.load('en_core_web_lg')
+
+
+doc = nlp(wines["description"][3])
+spacy.displacy.render(doc, style='ent',jupyter=True)
+
+punctuations = string.punctuation
+stopwords = list(STOP_WORDS)
+
+
+review = str(" ".join([i.lemma_ for i in doc]))
+
+doc = nlp(review)
+spacy.displacy.render(doc, style='ent',jupyter=True)
+
+
+# POS tagging
+for i in nlp(review):
+    print(i,"=>",i.pos_)
+
+# Parser for reviews
+parser = English()
+def spacy_tokenizer(sentence):
+    mytokens = parser(sentence)
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
+    mytokens = [ word for word in mytokens if word not in stopwords and word not in punctuations ]
+    mytokens = " ".join([i for i in mytokens])
+    return mytokens
+
+tqdm.pandas()
+wines["processed_description"] = wines["description"].progress_apply(spacy_tokenizer)
+
+
+# Creating a vectorizer
+vectorizer = CountVectorizer(min_df=5, max_df=0.9, stop_words='english', lowercase=True, token_pattern='[a-zA-Z\-][a-zA-Z\-]{2,}')
+data_vectorized = vectorizer.fit_transform(wines["processed_description"])
+
+NUM_TOPICS = 10
+
+# Latent Dirichlet Allocation Model
+lda = LatentDirichletAllocation(n_components=NUM_TOPICS, max_iter=10, learning_method='online',verbose=True)
+data_lda = lda.fit_transform(data_vectorized)
+
+# Non-Negative Matrix Factorization Model
+nmf = NMF(n_components=NUM_TOPICS)
+data_nmf = nmf.fit_transform(data_vectorized)
+
+# Latent Semantic Indexing Model using Truncated SVD
+lsi = TruncatedSVD(n_components=NUM_TOPICS)
+data_lsi = lsi.fit_transform(data_vectorized)
+
+# Functions for printing keywords for each topic
+def selected_topics(model, vectorizer, top_n=10):
+    for idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (idx))
+        print([(vectorizer.get_feature_names()[i], topic[i])
+                        for i in topic.argsort()[:-top_n - 1:-1]])
+
+# Keywords for topics clustered by Latent Dirichlet Allocation
+print("LDA Model:")
+selected_topics(lda, vectorizer)
+
+# Keywords for topics clustered by Latent Semantic Indexing
+print("NMF Model:")
+selected_topics(nmf, vectorizer)
+
+# Keywords for topics clustered by Non-Negative Matrix Factorization
+print("LSI Model:")
+selected_topics(lsi, vectorizer)
+
+# Transforming an individual sentence
+text = spacy_tokenizer("Aromas include tropical fruit, broom, brimstone and dried herb. The palate isn't overly expressive, offering unripened apple, citrus and dried sage alongside brisk acidity.")
+x = lda.transform(vectorizer.transform([text]))[0]
+print(x)
+
+
+# Visualizing LDA results with pyLDAvis
+
+pyLDAvis.enable_notebook()
+dash = pyLDAvis.sklearn.prepare(lda, data_vectorized, vectorizer, mds='tsne')
+dash
+
+
+# Visualizing LSI(SVD) scatterplot
+svd_2d = TruncatedSVD(n_components=2)
+data_2d = svd_2d.fit_transform(data_vectorized)
+
+trace = go.Scattergl(
+    x = data_2d[:,0],
+    y = data_2d[:,1],
+    mode = 'markers',
+    marker = dict(
+        color = '#FFBAD2',
+        line = dict(width = 1)
+    ),
+    text = vectorizer.get_feature_names(),
+    hovertext = vectorizer.get_feature_names(),
+    hoverinfo = 'text'
+)
+data = [trace]
+iplot(data, filename='scatter-mode')
+
+## The text version of scatter plot looks messy but you can zoom it for great results
+
+trace = go.Scattergl(
+    x = data_2d[:,0],
+    y = data_2d[:,1],
+    mode = 'text',
+    marker = dict(
+        color = '#FFBAD2',
+        line = dict(width = 1)
+    ),
+    text = vectorizer.get_feature_names()
+)
+data = [trace]
+iplot(data, filename='text-scatter-mode')
+
+
+def spacy_bigram_tokenizer(phrase):
+    doc = parser(phrase) # create spacy object
+    token_not_noun = []
+    notnoun_noun_list = []
+    noun = ""
+
+    for item in doc:
+        if item.pos_ != "NOUN": # separate nouns and not nouns
+            token_not_noun.append(item.text)
+        if item.pos_ == "NOUN":
+            noun = item.text
+
+        for notnoun in token_not_noun:
+            notnoun_noun_list.append(notnoun + " " + noun)
+
+    return " ".join([i for i in notnoun_noun_list])
+
+bivectorizer = CountVectorizer(min_df=5, max_df=0.9, stop_words='english', lowercase=True, ngram_range=(1,2))
+bigram_vectorized = bivectorizer.fit_transform(wines["processed_description"])
+
+## LDA for bigram data
+
+bi_lda = LatentDirichletAllocation(n_components=NUM_TOPICS, max_iter=10, learning_method='online',verbose=True)
+data_bi_lda = bi_lda.fit_transform(bigram_vectorized)
+
+### Topics for bigram model
+
+print("Bi-LDA Model:")
+selected_topics(bi_lda, bivectorizer)
+
+bi_dash = pyLDAvis.sklearn.prepare(bi_lda, bigram_vectorized, bivectorizer, mds='tsne')
+bi_dash
